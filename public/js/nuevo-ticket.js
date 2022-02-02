bf7cf38eff95de81f3de4a5075b5651ec986c17b
Jhon <ramirescortes@hotmail.com>
2022-02-02 15:44:02 -0500

    socket: next ticket

--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,30 @@
+// Referencias HTML
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket');
+const btnCrear = document.querySelector('button');
 
 
-console.log('Nuevo Ticket HTML');
+const socket = io();
+
+
+socket.on('connect', () => {
+    btnCrear.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnCrear.disabled = true;
+});
+
+//escuchando el ultimo ticket
+socket.on('ultimo-ticket', (ultimo) => {
+    lblNuevoTicket.innerText = 'Ticket ' + ultimo;
+})
+
+
+btnCrear.addEventListener( 'click', () => {
+
+   //generando nuevo ticket
+    socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+        lblNuevoTicket.innerText = ticket;
+    });
+
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -3,11 +3,18 @@ const TicketControl = require('../models/ticket-control');
 const ticketControl = new TicketControl();
 
 const socketController = (socket) =>{ // validacion del JWT
+
+
+       // Cuando un cliente se conecta
+       socket.emit( 'ultimo-ticket', ticketControl.ultimo );
+       socket.emit( 'estado-actual', ticketControl.ultimos4 );
+       socket.emit( 'tickets-pendientes', ticketControl.tickets.length);
   
-         socket.on('enviar-mensaje',(payload,callback) =>{
-             const id = 123456;
-             callback(id);
-            socket.broadcast.emit('enviar-mensaje', payload)
+         socket.on('siguiente-ticket',(payload,callback) =>{
+        
+            const siguiente = ticketControl.siguiente();
+            callback( siguiente );
+            socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
 
          })
          
