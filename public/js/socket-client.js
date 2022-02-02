85378f9ffaf2f0b3fa1c1f8a3f625407cd03c5d2
Jhon <ramirescortes@hotmail.com>
2022-02-01 20:53:41 -0500

    added span in the index.html and socket client

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,2 +1,22 @@
+//Referencias del HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+
 //socket cliente
 const socket = io();
+
+
+//listening events
+socket.on('connect',() =>{
+    console.log('conectado al servidor');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+    
+})
+
+socket.on('disconnect',() =>{
+    console.log('Desconectado del servidor');
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+})
