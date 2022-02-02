403f7b11a08f5b7b9e2c17d0db5e4a35fb726965
Jhon <ramirescortes@hotmail.com>
2022-02-02 16:15:03 -0500

    attend to ticket

--- sockets/controller.js
+++ sockets/controller.js
@@ -17,6 +17,36 @@ const socketController = (socket) =>{ // validacion del JWT
             socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
 
          })
+
+         socket.on('atender-ticket', ({ escritorio }, callback) => {
+        
+            if ( !escritorio ) {
+                return callback({
+                    ok: false,
+                    msg: 'Es escritorio es obligatorio'
+                });
+            }
+    
+            const ticket = ticketControl.atenderTicket( escritorio );
+    
+            
+            socket.broadcast.emit( 'estado-actual', ticketControl.ultimos4 );
+            socket.emit( 'tickets-pendientes', ticketControl.tickets.length);
+            socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
+    
+            if ( !ticket ) {
+                callback({
+                    ok: false,
+                    msg: 'Ya no hay tickets pendientes'
+                });
+            } else {
+                callback({
+                    ok: true,
+                    ticket
+                })
+            }
+    
+        })
          
      }
 
