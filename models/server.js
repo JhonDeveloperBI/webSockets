bdeb397a19406f5a3727815a7fb295da4cf0b04e
Jhon <ramirescortes@hotmail.com>
2022-02-02 14:18:58 -0500

    Organize code

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { socketController } = require('../sockets/controller');
 
 class Server {
 
@@ -42,29 +43,7 @@ class Server {
     }
 
     sockets(){
-        this.io.on('connection',socket =>{ // validacion del JWT
-       //    console.log('cliente conectado', socket.id);
-           socket.on('disconnect',() =>{
-          //   console.log('cliente desconectado',socket.id); // id volatiles
-           })
-          // return socket.disconect();
-
-            //escuchar mensaje
-            //callback referen the socket client enviar-mensaje
-            socket.on('enviar-mensaje',(payload,callback) =>{
-
-                const id = 123456;
-                callback(id);
-
-               // console.log('enviar mensaje desde el server', payload);
-               
-               // enviar mensaje a todos los clientes conectados
-
-             //  this.io.emit('enviar-mensaje', payload)
-
-            })
-            
-        });
+        this.io.on('connection',socketController);
     }
 
     listen() {
